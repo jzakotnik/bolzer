131ecd741e0a6ec7ff4cd37686f127779a829276
jzakotnik <jure@zakotnik.de>
2020-09-17 11:44:41 +0200

    Updates

---
+++ precache-manifest.03baafdabb8b3c202201eb6962e86cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbdedea7b920bea428f9dbe239e3fe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04ae23a209fd6890235f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7581ff393794b0067cd",
+    "url": "/static/js/2.78d98c8c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.78d98c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ae23a209fd6890235f",
+    "url": "/static/js/main.b7f9c08c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
